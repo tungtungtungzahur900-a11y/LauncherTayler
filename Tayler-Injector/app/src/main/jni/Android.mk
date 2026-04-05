c22cfdebd4dcaa9e95368671360dd77f2e867467
tungtungtungzahur900-a11y <tungtungtungzahur900@gmail.com>
2026-04-05 19:05:02 +0300

    Update Android.mk

--- Tayler-Injector/app/src/main/jni/Android.mk
+++ Tayler-Injector/app/src/main/jni/Android.mk
@@ -1,27 +1,30 @@
 LOCAL_PATH := $(call my-dir)
 
-# 1. Подключаем готовую либу Oxide
+# 1. Подключаем готовую библиотеку Oxide
 include $(CLEAR_VARS)
 LOCAL_MODULE := OxideInternal
 LOCAL_SRC_FILES := ../jniLibs/arm64-v8a/libOxideInternal.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-# 2. Собираем сам чит
+# 2. Собираем сам проект чита
 include $(CLEAR_VARS)
 LOCAL_MODULE := TaylerCheat
 
-# Указываем путь, где лежат .h файлы ImGui
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/imgui
+# Указываем компилятору искать файлы .h в папке jni и jni/imgui
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/imgui
 
-# Список всех твоих файлов + файлы ImGui
+# Список всех исходных файлов для сборки
 LOCAL_SRC_FILES := main.cpp \
                    MenuGUI.cpp \
                    OxideInternal.cpp \
                    imgui/imgui.cpp \
                    imgui/imgui_draw.cpp \
                    imgui/imgui_widgets.cpp \
-                   imgui/imgui_tables.cpp
+                   imgui/imgui_tables.cpp \
+                   imgui/imgui_demo.cpp
 
+# Системные библиотеки
 LOCAL_LDLIBS := -llog -landroid
 LOCAL_SHARED_LIBRARIES := OxideInternal
 
